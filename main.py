35ddf8ced56ba4743e2649b066354d0fab6543e3
sashachack <54422609+sashachack@users.noreply.github.com>
2021-01-20 23:24:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,6 @@ r.login(username, password)
 
 info = pd.read_csv(r'crypto_info.csv')
 
-# useful stuff:
-#   r.orders.get_all_open_crypto_orders()
-#   r.orders.order_buy_crypto_by_price()
-#   r.orders.order_sell_crypto_by_quantity()
-#   r.crypto.get_crypto_historicals()
-#   r.crypto.get_crypto_positions()
-#   r.crypto.get_crypto_quote()
-
 
 def get_open_positions():
     crypto_positions = r.crypto.get_crypto_positions()
@@ -70,26 +62,24 @@ def trade(all_coins, positions, moves):
         quant = float(positions[sym]) if sym in positions else 0
         worth = float(r.crypto.get_crypto_quote(sym)['mark_price']) * quant
         if move == 1:  # wanna be in this pos
-            print('Want to hold {}'.format(sym))
+            # print('Want to hold {}'.format(sym))
             if sym in positions:  # already in this pos
-                print('\tAlready holding {}'.format(sym))
+                # print('\tAlready holding {}'.format(sym))
                 continue
             else:  # time to buy into the pos
                 old_worth = round(float(info.at[0, sym]), 2)
-                # if sym == 'DOGE':
-                #     old_worth = round(old_worth, 8)
-                print('\tLet\'s buy {} worth of {}'.format(old_worth, sym))
+                print('Let\'s buy {} worth of {}'.format(old_worth, sym))
                 # buy old_worth amt
-                print(r.orders.order_buy_crypto_by_price(sym, old_worth))
+                r.orders.order_buy_crypto_by_price(sym, old_worth)
                 info.at[0, sym] = 0
         else:  # don't wanna be in this pos
-            print('Don\'t want to hold {}'.format(sym))
+            # print('Don\'t want to hold {}'.format(sym))
             if sym not in positions:  # not in this pos
-                print('\tNot holding {}'.format(sym))
+                # print('\tNot holding {}'.format(sym))
                 continue
             else:  # time to get out of this pos
-                print('\tLet\'s sell {:.4f} {}'.format(quant, sym))
-                print(r.orders.order_sell_crypto_by_quantity(sym, round(quant, 8)))
+                print('Let\'s sell {:.4f} {}'.format(quant, sym))
+                r.orders.order_sell_crypto_by_quantity(sym, round(quant, 8))
                 info.at[0, sym] = round(worth, 2)
                 # sell all of it
     info.to_csv(r'crypto_info.csv', index=False)
